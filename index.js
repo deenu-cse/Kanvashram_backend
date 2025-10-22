72f39491dcf9b9daa0aba2289ed3a91d53819edf
deenu-cse <236301074@gkv.ac.in>
2025-10-22 22:03:47 +0530

    solve deployment errors

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const serverless = require('serverless-http');
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -28,19 +28,13 @@ app.use('/auth', require('./routes/auth'));
 app.use('/rooms', require('./routes/rooms'));
 app.use('/bookings', require('./routes/bookings'));
 app.use('/admins', require('./routes/admins'));
-
-//userRoutes
-
-app.use('/user', require('./userRoutes/bookingRoutes'))
-app.use('/user', require('./userRoutes/roomBooking'))
-app.use('/user', require('./userRoutes/auth'))
-
+app.use('/user', require('./userRoutes/bookingRoutes'));
+app.use('/user', require('./userRoutes/roomBooking'));
+app.use('/user', require('./userRoutes/auth'));
 app.use('/registrations', require('./routes/registration'));
-
 app.use('/donations', require('./routes/donations'));
 app.use('/payments', require('./routes/payments'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Instead of app.listen(), export the handler
+module.exports = app;
+module.exports.handler = serverless(app);
