112d38036ab8ab1a0a03cf6e959c56cc69e01941
deenu-cse <236301074@gkv.ac.in>
2025-10-24 15:19:46 +0530

    render deploy code

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ const PORT = process.env.PORT || 5000;
 const { dbConnect } = require('./config/db');
 dbConnect();
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
